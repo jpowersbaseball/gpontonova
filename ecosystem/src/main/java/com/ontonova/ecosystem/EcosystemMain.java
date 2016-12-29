11ec830a94c1e2f0114fede0c7ab704a2746d7d2
Josh Powers <jpowersbaseball@yahoo.com>
2016-12-29 13:06:33 -0500

    started the project

---
+++ ecosystem/src/main/java/com/ontonova/ecosystem/EcosystemMain.java
@@ -0,0 +1,12 @@
+package com.ontonova.ecosystem;
+
+public class EcosystemMain {
+
+  /**
+   * Various main tests for different ecosystem functions
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+  }
+}
